7dd8a2117ab6fef95f167dcc03fa882ef1f734e9
iqbalhasandev <iqbalhasan.dev@gmail.com>
2019-08-31 23:15:56 +0600

    Css footer add

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        margin: 10,
+        loop: true,
+        autoplay: true,
+        smartSpeed: 500,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+    });
+});

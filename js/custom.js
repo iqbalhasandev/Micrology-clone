78b2c200bf0710481ffacb1e4e23c82c64e18db8
iqbalhasandev <iqbalhasan.dev@gmail.com>
2019-09-01 20:53:28 +0600

    responcive css add

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
+    $("#our-work-slider").owlCarousel({
         margin: 10,
         loop: true,
         autoplay: true,
@@ -8,5 +8,31 @@ $(document).ready(function () {
         autoplayHoverPause: true,
         nav: true,
         dots: false,
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1,
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 2,
+            },
+            // breakpoint from 768 up
+            768: {
+                items: 3,
+            }
+        }
+    });
+
+    $("#testimonial-slider").owlCarousel({
+        margin: 10,
+        loop: true,
+        autoplay: true,
+        smartSpeed: 500,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        items: 1,
     });
 });
+
